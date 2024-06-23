525b30b90b996ba7d2b732fb38bf5359b08a643b
RYAN LEE <ryan_lee@live.com>
2024-06-23 14:53:43 -0600

    Moved repository to new directory

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+public static class SimpleServerlessApp
+{
+    [FunctionName("HelloWorld")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        string name = req.Query["name"];
+
+        return new OkObjectResult($"Hello, {name}! This is your simple serverless app.");
+    }
+}
